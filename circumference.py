99f19e243e5c71f73445acd56d20f0a3e75caa37
DeathPanda303 <james.neo.leet@gmail.com>
2016-01-31 11:24:57 -0600

    Create circumference.py

---
+++ circumference.py
@@ -0,0 +1,15 @@
+# Compute the circumference of a circle, given the length of its radius.
+
+###################################################
+# Circle circumference formula
+# Student should enter statement on the next line.
+radius = 8
+pi = 3.14
+print 2*(radius*pi)
+
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#50.24
