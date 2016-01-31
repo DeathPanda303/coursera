3d651a691626372b58705022dd1f31d06b68e5b2
DeathPanda303 <james.neo.leet@gmail.com>
2016-01-31 11:27:18 -0600

    Create time_conversion.py

---
+++ time_conversion.py
@@ -0,0 +1,21 @@
+# Compute the number of seconds in a given number of hours, minutes, and seconds.
+
+###################################################
+# Hours, minutes, and seconds to seconds conversion formula
+# Student should enter statement on the next line.
+#7 hours, 21 minutes and 37 seconds.
+SecondInMinute = 60
+MinuteInHour = 60
+MinuteInHour *= 7
+Mins = 21
+Mins *= 60
+SecondInMinute *= MinuteInHour
+SecondInMinute += 37
+SecondInMinute += Mins
+print SecondInMinute
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#26497
