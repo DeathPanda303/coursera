e0a983cd52c7cb2a6a380fd9bf5d04494ef3b681
DeathPanda303 <james.neo.leet@gmail.com>
2016-01-31 11:26:38 -0600

    Create miles_feet.py

---
+++ miles_feet.py
@@ -0,0 +1,15 @@
+# Compute the number of feet corresponding to a number of miles.
+
+###################################################
+# Miles to feet conversion formula
+# Student should enter statement on the next line.
+FeetInMiles = 5280
+Miles = 13
+FeetInMiles *= Miles
+print FeetInMiles
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#68640
